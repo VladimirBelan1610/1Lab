dd259da52967dd7dd4ae81bb3911cf6a0c78c2f9
VladimirBelan1610 <v.i.belan@viarune.com>
2024-05-24 11:06:31 +0700

    Make new realisation

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include "Logger.h"
 #include <locale.h>
 #include <inputreader.h>
+
 int main(int argc, char *argv[])
 {
+
     setlocale(LC_ALL,"ru_RU.UTF-8");
 
     QCoreApplication a(argc, argv);
@@ -14,6 +16,7 @@ int main(int argc, char *argv[])
     FileWatcher FileWatcher; //созданы объекты класса
     Logger Logger;
 
+    QObject::connect(&FileWatcher,&FileWatcher::LoggerRequest,&Logger, &Logger::Output);
 
     Logger.Input(1);
     QString path = InputReader.ReadInput();
